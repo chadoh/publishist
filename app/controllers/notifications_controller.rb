2b09db2aeee95f8d841ab0c7f74427d4d8379dbd
Chad Ostrowski <chad.ostrowski@gmail.com>
2010-07-29 08:03:57 -0400

    html emails are looking deese. Finally. Now on to user accounts.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,5 +4,9 @@ class NotificationsController < ApplicationController
     @composition_body = "and her infinite taste"
     @author = "Merry Berry"
     @url = "#"
+
+    @green = "#8daf25"
+    @blue = "#8bc7c7"
+    @font_family = "'Helvetica Neue', Arial, Helvetica, sans-serif"
   end
 end
