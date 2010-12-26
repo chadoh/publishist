9ebc9317355cd6ccd90f511e77b3bf7bf49d0663
Chad Ostrowski <chad.ostrowski@gmail.com>
2010-12-25 20:57:52 -0500

    the packet for a meeting can now be sorted as expected

--- public/javascripts/meetings.js
+++ public/javascripts/meetings.js
@@ -8,11 +8,10 @@ $(function(){
         $('span.name').html($(this).val().split(' ')[0].replace(/"/g, '') + "'s");
     });
 
-  $('span.drag-handle').css('display', 'inline-block')
   $('ol.packets').sortable({
     axis: 'y',
     items: 'li',
-    handle: 'span.drag-handle',
+    handle: 'span.drag-handle-wrap',
     update: function(event, ui){
       li = ui.item;
       packet_id = li.attr('id').split('_')[1];
