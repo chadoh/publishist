f3467f1103959707a42d8e83cc0c4bd4b80c3e8d
Chad Ostrowski <chad.ostrowski@gmail.com>
2011-01-14 17:41:32 -0500

    the front-end for entering bulk scores works; meetings#show not so much

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,12 +1,14 @@
 class MeetingsController < InheritedResources::Base
   before_filter :authenticate_person!, :only => [:index, :show]
   before_filter :editors_only, :except => [:index, :show]
+  before_filter :coeditor_only, :only => :scores
   before_filter :resource, :only => :scores
 
   actions :index, :show, :new, :create, :update, :destroy, :scores
 
   def show
-    @show_score = if person_viewing_attended? then true else false end
+    @show_score = if person_viewing_attended? and editor_didnt_enter_their_score?
+      then true else false end
     @show_author = false
     @attendance = Attendance.new
     show!
@@ -18,8 +20,17 @@ class MeetingsController < InheritedResources::Base
 
 protected
 
+  def viewers_attendance
+    @viewers_attendance ||= resource.attendances.select  do |a|
+      a.person == current_person
+    end
+  end
+
   def person_viewing_attended?
-    resource.attendances.select {|a| a.person == current_person }
+    viewers_attendance.present?
+  end
+
+  def editor_didnt_enter_their_score?
   end
 
   def resource
