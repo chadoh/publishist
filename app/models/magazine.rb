f0ae4e19e0acd24273163e3d0676197b3cfbbe77
Chad Ostrowski <chad.ostrowski@gmail.com>
2011-02-23 08:30:25 -0500

    More validation for magazine creation

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -3,20 +3,50 @@ class Magazine < ActiveRecord::Base
   validates_presence_of :accepts_submissions_from
   validates_presence_of :accepts_submissions_until
   validate :from_happens_before_until
+  validate :magazine_ranges_dont_conflict
 
   after_initialize "self.nickname = 'next' if self.nickname.blank?"
-  after_initialize "self.accepts_submissions_from = Date.today if self.accepts_submissions_from.blank?"
-  after_initialize "self.accepts_submissions_until = Date.today + 6.months if self.accepts_submissions_until.blank?"
+  after_initialize :accepts_from_after_latest_or_perhaps_today
+  after_initialize :accepts_until_six_months_later
 
   default_scope order("accepts_submissions_until DESC")
 
 protected
-  
+
+  def accepts_from_after_latest_or_perhaps_today
+    if self.accepts_submissions_from.blank?
+      if Magazine.all.present?
+        self.accepts_submissions_from = Magazine.order("accepts_submissions_until DESC").first.accepts_submissions_until
+      else
+        self.accepts_submissions_from = Date.today 
+      end
+    end
+  end
+
+  def accepts_until_six_months_later
+    self.accepts_submissions_until = self.accepts_submissions_from + 6.months if self.accepts_submissions_until.blank?
+  end
+
   def from_happens_before_until
     if !!self.accepts_submissions_until && !!self.accepts_submissions_from
       if self.accepts_submissions_until <= self.accepts_submissions_from
         errors.add(:accepts_submissions_until, "must come after \"Accepts submissions from\"")
       end
+    end
+  end
+
+  def magazine_ranges_dont_conflict
+    if Magazine.where(
+      :accepts_submissions_from  < self.accepts_submissions_from,
+      :accepts_submissions_until > self.accepts_submissions_from
+    ).present?
+      then errors.add :accepts_submissions_from,  "can't occurr during another magazine"
+    end
+    if Magazine.where(
+      :accepts_submissions_from  < self.accepts_submissions_until,
+      :accepts_submissions_until > self.accepts_submissions_until
+    ).present?
+      then errors.add :accepts_submissions_until, "can't occurr during another magazine"
     end
   end
 end
--- spec/models/magazine_spec.rb
+++ spec/models/magazine_spec.rb
@@ -15,24 +15,51 @@ describe Magazine do
   end
 
   describe "#accepts_submissions_from" do
-    it "defaults to today" do
-      mag = Magazine.new
-      mag2 = Magazine.new :accepts_submissions_from => Date.today
-      mag.accepts_submissions_from.should == mag2.accepts_submissions_from
+    context "when this is the first magazine" do
+      it "defaults to today" do
+        mag = Magazine.new
+        mag2 = Magazine.new :accepts_submissions_from => Date.today
+        mag.accepts_submissions_from.should == mag2.accepts_submissions_from
+      end
+    end
+
+    context "when there are other magazines" do
+      it "defaults to the accepts_submissions_until date of the latest magazine" do
+        mag = Magazine.create
+        mag2 = Magazine.new
+        mag2.accepts_submissions_from.should == mag.accepts_submissions_until
+      end
+    end
+
+    it "cannot fall within another magazine's range" do
+      orig = Magazine.create
+      mag = Magazine.new :accepts_submissions_from => orig.accepts_submissions_from + 1.day
+      mag.should_not be_valid
     end
   end
 
   describe "#accepts_submissions_until" do
-    it "defaults to today" do
-      mag = Magazine.new
-      mag2 = Magazine.new :accepts_submissions_until => Date.today + 6.months
-      mag.accepts_submissions_until.should == mag2.accepts_submissions_until
+    it "defaults to six months after accepts_submissions_from" do
+      mag = Magazine.create
+      mag.accepts_submissions_until.should == mag.accepts_submissions_from + 6.months
+      # need to test another one, since the way `from` initializes changes with subsequent mags
+      mag2 = Magazine.new
+      mag2.accepts_submissions_until.should == mag2.accepts_submissions_from + 6.months
     end
 
     it "must be > self.accepts_submissions_from" do
       mag = Magazine.new(
         :accepts_submissions_from  => Date.today,
         :accepts_submissions_until => Date.yesterday
+      )
+      mag.should_not be_valid
+    end
+
+    it "cannot fall within another magazine's range" do
+      orig = Magazine.create
+      mag = Magazine.new(
+        :accepts_submissions_until => orig.accepts_submissions_until - 1.day,
+        :accepts_submissions_from => Date.yesterday
       )
       mag.should_not be_valid
     end
