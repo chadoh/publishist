2d1ad0fe9b3d8c04000f91122a6a0e28792b639e
Chad Ostrowski <chad.ostrowski@gmail.com>
2011-03-19 12:48:55 -0400

    The magazine#highest_scores page is lookin' pretty

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -2,6 +2,8 @@ class MagazinesController < InheritedResources::Base
   before_filter :authenticate_person!
   before_filter :editors_only, :except => [:index]
 
+  custom_actions :resource => :highest_scores
+
   def create
     create!(:notice => nil) { magazines_path }
   end
@@ -11,7 +13,11 @@ class MagazinesController < InheritedResources::Base
   end
 
   def highest_scores
-    @submissions = resource.highest_scores(params[:top].presence || 50)
+    # TODO: chance this to `resource.submissions.count` when nested hm:t support is added
+    @max = resource.meetings.collect(&:submissions).flatten.uniq.count
+    @default = [@max * 3 / 4, 50].min
+    @highest = params[:highest].presence || (@max * 3 / 4)
+    @submissions = resource.highest_scores @highest.to_i
   end
 
 end
