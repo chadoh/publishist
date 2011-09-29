e6321d071fe5d1f7bd9ae256b747791c3ef6f5dd
Chad Ostrowski <chad.ostrowski@gmail.com>
2011-09-29 17:23:39 -0400

    Spork is working against me, not with me.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,48 +1,40 @@
-require 'rubygems'
-require 'spork'
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'email_spec'
 
-Spork.prefork do
-  # This file is copied to spec/ when you run 'rails generate rspec:install'
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  require 'email_spec'
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  config.mock_with :rspec
 
-  RSpec.configure do |config|
-    # == Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
-    config.mock_with :rspec
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = false
 
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    config.use_transactional_fixtures = false
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
 
-    config.include(EmailSpec::Helpers)
-    config.include(EmailSpec::Matchers)
-
-    config.before(:suite) do
-        DatabaseCleaner.strategy = :truncation
-    end
-
-    config.before(:each) do
-        DatabaseCleaner.start
-    end
-
-    config.after(:each) do
-        DatabaseCleaner.clean
-    end
+  config.before(:suite) do
+      DatabaseCleaner.strategy = :truncation
   end
-end
 
-Spork.each_run do
+  config.before(:each) do
+      DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+      DatabaseCleaner.clean
+  end
 end
