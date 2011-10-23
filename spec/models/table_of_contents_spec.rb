dea561a0899ebdee21ac8c29cc720a59c95b1928
Chad Ostrowski <chad.ostrowski@gmail.com>
2011-10-23 08:55:19 -0400

    Fixed a broken spec and styled something pretty.

--- spec/models/table_of_contents_spec.rb
+++ spec/models/table_of_contents_spec.rb
@@ -8,7 +8,7 @@ describe TableOfContents do
       :nickname                  => "Fruit Blots"
     )
     @submission  = Factory.create(:submission)
-    @submission2 = Factory.create(:submission)
+    @submission2 = Factory.create(:submission_with_signed_in_author)
     @meeting     = Meeting.create(datetime: 2.weeks.ago, question: "orly?")
     @meeting.submissions << [@submission, @submission2]
     @magazine.publish [@submission, @submission2]
