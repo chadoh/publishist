16007bacc88bad6471217c2e178d36dfe0485a26
Chad Ostrowski <hi@chadoh.com>
2013-06-27 00:43:36 -0400

    remove precompiled assets

--- public/assets/highest_scores-4ee53a54c021e340c46929526f4cc0dd.js
+++
@@ -1,1 +0,0 @@
-function clearIrrelevantInput(t){t.attr("id")=="highest"?$("form#highest input#above").val(""):$("form#highest input#highest").val("")}$(function(){$("form#highest input[type=number]").blur(function(){clearIrrelevantInput($(this))}),$("form#highest input[type=submit]").submit(function(){clearIrrelevantInput($(this))})});
