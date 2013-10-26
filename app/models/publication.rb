104e8c94645f9c422e499c8b3dd02a76e2ffdfa6
Chad Ostrowski <hi@chadoh.com>
2013-10-26 12:51:54 -0400

    Clean up publications form

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,5 +1,5 @@
 class Publication < ActiveRecord::Base
-  attr_accessible :address, :subdomain, :latitude, :longitude, :name, :tagline, :about, :meetings_info, :publication_detail_attributes, :custom_domain
+  attr_accessible :address, :subdomain, :latitude, :longitude, :name, :tagline, :about, :meetings_info, :publication_detail_attributes, :custom_domain, :meta_description
 
   has_many :magazines, dependent: :destroy, inverse_of: :publication
   has_many :submissions, dependent: :destroy
